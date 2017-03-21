daf6159763c5814390718af85c545f64637f78a6
Erwan Guillou <erwan.guillou@liris.cnrs.fr>
2017-03-21 13:02:47 +0100

    update install config file for package

--- cmake/ScreepsApiV12-Config.cmake
+++ cmake/ScreepsApiV12-Config.cmake
@@ -1,5 +1,5 @@
 
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-get_filename_component(ScreepsApiV12_INCLUDE_DIRS "${SELF_DIR}/../../include" ABSOLUTE)
+get_filename_component(ScreepsApiV12_INCLUDE_DIRS "${SELF_DIR}/../include" ABSOLUTE)
 get_filename_component(ScreepsApiV12_LIBRARY_DIRS "${SELF_DIR}/../lib" ABSOLUTE)
 set(ScreepsApiV12_LIBRARIES "${CMAKE_SHARED_LIBRARY_PREFIX}ScreepsApiV12${CMAKE_SHARED_LIBRARY_SUFFIX}")
